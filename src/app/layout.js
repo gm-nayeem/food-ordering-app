6cc86aaa1254614feebc0f70b49d61edf3e1e365
golam mostafa <gmnayeem25@gmail.com>
2024-01-02 18:51:25 +0600

    create structures both api and client

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
           <Toaster />
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="border-t p-8 text-center text-gray-500 mt-16">
-            &copy; 2023 All rights reserved
+            All rights reserved by &copy; 2023 Golam Mostafa.
         </footer>
     )
 }
